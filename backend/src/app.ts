4a42f8daa4acdeddc455255aa9442a75d3554e01
Sunnith <111701116+sunnithdev@users.noreply.github.com>
2025-11-17 15:54:24 -0500

    Allow origin access

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://sr-ed-ai.netlify.app', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Clerk middleware
